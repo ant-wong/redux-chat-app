48e8d46e1194a31054ba6006490414c87592ad1a
Anthony Wong <anthony.wong182@gmail.com>
2018-05-12 22:08:20 -0700

    sages and websocket server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import setUpSocket from './sockets/'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import username from './utils/name'
 
-import chat from './reducers'
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducers, applyMiddleware(sagaMiddleware))
 
-const store = createStore(chat)
+const socket = setUpSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, {socket, username})
 
 ReactDOM.render(
   <Provider store={store}>
